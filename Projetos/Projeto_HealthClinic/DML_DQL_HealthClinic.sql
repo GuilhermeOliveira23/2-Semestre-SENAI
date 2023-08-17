e0ce65b3bdc92f51f1b363ab9ce98c9b08924346
Guilherme Gozzi <olivergozzi@gmail.com>
2023-08-17 15:15:45 -0300

    Projeto HealthClinic - Documentação

--- Projetos/Projeto_HealthClinic/DML_DQL_HealthClinic.sql
+++ Projetos/Projeto_HealthClinic/DML_DQL_HealthClinic.sql
@@ -46,17 +46,7 @@ where idUsuario = 5
 
 
 
-   Create Procedure MedicosDaEspecialidade
- @TituloEspecialidade  Varchar(80)
- as
- Set @TituloEspecialidade = '%' + @TituloEspecialidade+ '%'
-  Select TituloEspecialidade, Nome
- from Especialidade e
- left join Medico m on (m.IdEspecialidade = e.IdEspecialidade)
- left join Usuario u on ( u.IdUsuario = m.IdUsuario )
- Where e.TituloEspecialidade like @TituloEspecialidade 
 
-Exec MedicosDaEspecialidade 'Neurologia'
 
 
 
